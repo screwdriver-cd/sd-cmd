7fbf0a69db24951b6fc73852ab6b061b01c55e70
ysawada <ysawada@yahoo-corp.jp>
2018-01-11 15:14:48 +0900

    add SD_ARTIFACT config

--- config/config.go
+++ config/config.go
@@ -1,6 +1,9 @@
 package config
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 var (
 	// VERSION is this cli version
@@ -11,15 +14,28 @@ var (
 	SDStoreURL string
 	// SDToken is SD_TOKEN value
 	SDToken string
+	// SDArtifactsDir is SD_ARTIFACTS_DIR value
+	SDArtifactsDir string
 	// BaseCommandPath is path of installing binary command
 	BaseCommandPath = "/opt/sd/commands/"
 )
 
+func addSlash(val string) string {
+	if val == "" {
+		return val
+	}
+	if val[len(val)-1] != '/' {
+		return fmt.Sprintf("%s/", val)
+	}
+	return val
+}
+
 // LoadConfig sets config data
 func LoadConfig() {
-	SDAPIURL = os.Getenv("SD_API_URL")
-	SDStoreURL = os.Getenv("SD_STORE_URL")
+	SDAPIURL = addSlash(os.Getenv("SD_API_URL"))
+	SDStoreURL = addSlash(os.Getenv("SD_STORE_URL"))
 	SDToken = os.Getenv("SD_TOKEN")
+	SDArtifactsDir = addSlash(os.Getenv("SD_ARTIFACTS_DIR"))
 	if VERSION == "" {
 		VERSION = "0.0.0"
 	}
--- config/config_test.go
+++ config/config_test.go
@@ -1,1 +1,56 @@
 package config
+
+import (
+	"os"
+	"testing"
+)
+
+var (
+	cacheEnv map[string]string
+)
+
+const (
+	dummyAPIURL   = "dummy-api"
+	dummyToken    = "dummy-token"
+	dummyStoreURL = "dummy-store/"
+)
+
+func setEnv(key, value string) {
+	if cacheEnv == nil {
+		cacheEnv = make(map[string]string)
+	}
+	cacheEnv[key] = os.Getenv(key)
+	os.Setenv(key, value)
+}
+
+func setup() {
+	setEnv("SD_API_URL", dummyAPIURL)
+	setEnv("SD_TOKEN", dummyToken)
+	setEnv("SD_STORE_URL", dummyStoreURL)
+}
+
+func teardown() {
+	for key, val := range cacheEnv {
+		os.Setenv(key, val)
+	}
+}
+
+func TestLoadConfig(t *testing.T) {
+	LoadConfig()
+	if SDAPIURL != "dummy-api/" {
+		t.Errorf("SDAPIURL=%q, want %q", SDAPIURL, "dummy-api/")
+	}
+	if SDToken != dummyToken {
+		t.Errorf("SDAPIURL=%q, want %q", SDToken, dummyToken)
+	}
+	if SDStoreURL != dummyStoreURL {
+		t.Errorf("SDAPIURL=%q, want %q", SDStoreURL, dummyStoreURL)
+	}
+}
+
+func TestMain(m *testing.M) {
+	setup()
+	ret := m.Run()
+	teardown()
+	os.Exit(ret)
+}
