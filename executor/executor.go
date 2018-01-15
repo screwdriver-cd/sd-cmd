e18af293080071f5b13dc746044212f138d13e21
ysawada <ysawada@yahoo-corp.jp>
2018-01-15 15:10:25 +0900

    fix command execute log

--- executor/executor.go
+++ executor/executor.go
@@ -1,9 +1,10 @@
 package executor
 
 import (
-	"bufio"
 	"fmt"
+	"io"
 	"log"
+	"os"
 	"os/exec"
 	"path/filepath"
 	"time"
@@ -58,65 +59,15 @@ func New(sdAPI api.API, args []string) (Executor, error) {
 	return nil, nil
 }
 
-func writeCommandLog(count int, content chan string, finish chan bool, done chan bool) {
-	laggerCommand := new(logger.Logger)
-	laggerCommand.SetInfos(lagger.File, 0, true)
-	for {
-		select {
-		case c := <-content:
-			laggerCommand.Debug.Printf("%v", c)
-		case fin := <-finish:
-			if fin {
-				count--
-			}
-		}
-		if count <= 0 {
-			break
-		}
-	}
-	done <- true
-}
-
 func execCommand(path string, args []string) error {
 	cmd := exec.Command(path, args...)
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		return fmt.Errorf("failed to create stdout pipe for exec command: %v", err)
-	}
-
-	stderr, err := cmd.StderrPipe()
-	if err != nil {
-		return fmt.Errorf("failed to create stderr pipe for exec command: %v", err)
-	}
-
-	content := make(chan string)
-	finish := make(chan bool)
-	done := make(chan bool)
 	lagger.Debug.Println("mmmmmm START COMMAND OUTPUT mmmmmm")
-	go func(content chan string, finish chan bool) {
-		scanner := bufio.NewScanner(stdout)
-		for scanner.Scan() {
-			content <- fmt.Sprintf("%v\n", scanner.Text())
-		}
-		finish <- true
-	}(content, finish)
 
-	go func(content chan string, finish chan bool) {
-		scanner := bufio.NewScanner(stderr)
-		for scanner.Scan() {
-			content <- fmt.Sprintf("%v\n", scanner.Text())
-		}
-		finish <- true
-	}(content, finish)
+	cmd.Stdout = io.MultiWriter(lagger.File, os.Stderr)
+	cmd.Stderr = cmd.Stdout
 
-	go writeCommandLog(2, content, finish, done)
+	err := cmd.Run()
 
-	err = cmd.Run()
-
-	<-done
-	close(content)
-	close(finish)
-	close(done)
 	lagger.Debug.Println("mmmmmm FINISH COMMAND OUTPUT mmmmmm")
 	state := cmd.ProcessState
 	lagger.Debug.Printf("System Time: %v, User Time: %v\n", state.SystemTime(), state.UserTime())
