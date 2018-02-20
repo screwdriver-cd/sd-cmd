3bfebf4f6b46749671839d555e97924da498c269
yoshwata <yoshwata@yahoo-corp.jp>
2018-02-20 14:04:32 +0900

    fix build error

--- executor/binary.go
+++ executor/binary.go
@@ -18,7 +18,7 @@ type Binary struct {
 }
 
 // NewBinary returns Binary object
-func NewBinary(spec *util.Command, arg []string) (*Binary, error) {
+func NewBinary(spec *util.CommandSpec, arg []string) (*Binary, error) {
 	storeapi, err := store.New(config.SDStoreURL, spec, config.SDToken)
 	if err != nil {
 		return nil, err
